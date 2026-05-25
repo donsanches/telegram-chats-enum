b2bd84984d34e2a5a99bc549c91a9aa772c66d62
Don Sanches <187512896+donsanches@users.noreply.github.com>
2026-05-25 23:22:07 +0300

    Converted from sync to async

--- chatsenum.py
+++ chatsenum.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+import asyncio
 import configparser
-from telethon.sync import TelegramClient
+from telethon import TelegramClient
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -9,9 +10,14 @@ api_id   = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 session  = config['Telegram']['session']
 
-client = TelegramClient(session, api_id, api_hash)
-client.start()
 
-print("Fetching chat details...\n")
-for dialog in client.iter_dialogs():
-    print(f"Chat Name: {dialog.name}, Chat ID: {dialog.id}")
+async def main():
+    async with TelegramClient(session, api_id, api_hash) as client:
+        await client.start()
+        print("Fetching chat details...\n")
+        async for dialog in client.iter_dialogs():
+            print(f"Chat Name: {dialog.name}, Chat ID: {dialog.id}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
